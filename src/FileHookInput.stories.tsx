b075c7f1ee98d6112b75b51a9b5cb02b21be2bc0
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-10-28 17:59:50 +0200

    dev: integration with react-hook-forms

--- src/FileHookInput.stories.tsx
+++ src/FileHookInput.stories.tsx
@@ -2,14 +2,25 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { FileHookInput } from "./FileHookInput";
+import { useForm } from "react-hook-form";
 
 export default {
     title: "FileHookInput",
     component: FileHookInput,
 } as ComponentMeta<typeof FileHookInput>;
 
-const Template: ComponentStory<typeof FileHookInput> = (args) => (
-    <FileHookInput {...args} />
-);
+const Template: ComponentStory<typeof FileHookInput> = (args) => {
+    const { setValue, control } = useForm();
+
+    console.log("Args", args.name);
+
+    return <FileHookInput {...args} setValue={setValue} control={control} />;
+};
 
 export const Primary = Template.bind({});
+
+Primary.args = {
+    ...Primary.args,
+    name: "test",
+    multiple: true,
+};
--- src/FileHookInput.tsx
+++ src/FileHookInput.tsx
@@ -1,7 +1,68 @@
 import React from "react";
+import { Control, Controller, UseFormSetValue } from "react-hook-form";
 
-interface FileHookInputProps {}
+type FileHookInputProps = {
+    className?: string;
+    multiple?: boolean;
+    name: string;
+    control: Control<any>;
+    setValue: UseFormSetValue<any>;
+};
 
-export const FileHookInput = ({}: FileHookInputProps) => {
-    return <h1>It is supposed to be file</h1>;
+type File = {
+    name: string;
+    size: number;
+    type: string;
+    base64: string;
+};
+
+export const FileHookInput = ({
+    className,
+    multiple = false,
+    name,
+    control,
+    setValue,
+}: FileHookInputProps) => {
+    const fileChangeHandler: React.ChangeEventHandler<HTMLInputElement> = (
+        data
+    ) => {
+        const reader = new FileReader();
+
+        if (data.target.files && data.target.files[0]) {
+            const file = data.target.files[0];
+
+            reader.readAsDataURL(file);
+            reader.onload = () => {
+                const { name: fileName, size, type } = file;
+                const result = reader.result;
+
+                if (result && typeof result === "string") {
+                    setValue(name, {
+                        name: fileName,
+                        size,
+                        type,
+                        base64: result.split(",").pop(),
+                    } as File);
+                }
+            };
+        }
+    };
+
+    return (
+        <Controller
+            name={name}
+            control={control}
+            render={({ field: { onBlur, name, ref } }) => (
+                <input
+                    className={className}
+                    name={name}
+                    ref={ref}
+                    onChange={fileChangeHandler}
+                    onBlur={onBlur}
+                    type="file"
+                    multiple={multiple}
+                />
+            )}
+        />
+    );
 };
